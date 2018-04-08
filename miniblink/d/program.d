318d9705f9f2fb3a940297b5663197333a054680
wiffel <wiffel@tilient.net>
2018-04-08 23:25:32 +0200

    version with timer

--- miniblink/d/program.d
+++ miniblink/d/program.d
@@ -1,14 +1,26 @@
 import api;
 
-void gpioSetup()
-{
-	rcc_periph_clock_enable(rcc_periph_clken.RCC_GPIOC);
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+extern(C) void d_main() {
+  gpioSetup();
+  timerSetup();
+  for (;;) {
+    gpio_toggle(GPIOC, GPIO13);
+    wfi();
+  }
+}
+
+extern(C) void tim2_isr() {
+  if (timer_get_flag(TIM2, TIM_SR_CC1IF))
+    timer_clear_flag(TIM2, TIM_SR_CC1IF);
+}
+
+void gpioSetup() {
+  rcc_periph_clock_enable(rcc_periph_clken.RCC_GPIOC);
+  gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
                 GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 }
 
-void timerSetup()
-{
+void timerSetup() {
   rcc_periph_clock_enable(rcc_periph_clken.RCC_TIM2);
   nvic_enable_irq(NVIC_TIM2_IRQ);
   rcc_periph_reset_pulse(rcc_periph_rst.RST_TIM2);
@@ -23,18 +35,3 @@ void timerSetup()
   timer_enable_irq(TIM2, TIM_DIER_CC1IE);
 }
 
-extern(C) void tim2_isr()
-{
-  if (timer_get_flag(TIM2, TIM_SR_CC1IF))
-    timer_clear_flag(TIM2, TIM_SR_CC1IF);
-}
-
-extern(C) void d_main()
-{
-  gpioSetup();
-  timerSetup();
-  for (;;) {
-    gpio_toggle(GPIOC, GPIO13);
-    wfi();
-  }
-}
