51bfdc0869d9973efc507d4c9985cb432a9c6a90
wiffel <wiffel@tilient.net>
2018-05-06 00:56:10 +0200

    refactoring oled

--- miniblink/d/blink.d
+++ miniblink/d/blink.d
@@ -7,74 +7,38 @@ enum ledPin = GPIO13;
 
 extern(C) void main()
 {
-  import std.range: iota, chain, retro, drop;
-
   rcc_clock_setup_in_hse_8mhz_out_72mhz();
-  ledSetup();
   timerSetup();
+  ledSetup();
 
-  i2cSetup();
-  ssd1306Init();
-
-  enum r = iota(1, 33);
-  enum wave = r.chain(r.retro.drop(1));
-  foreach(_; 0 .. 10)
-    foreach(x; wave) {
-      ssd1306ClearScreen();
-      ssd1306FillRect(x, x, 80, 30, WHITE);
-      sleep(100);
-      ssd1306Refresh();
-      ssd1306FillRect(x+5, x+5, 70, 20, BLACK);
-      sleep(100);
-      ssd1306Refresh();
-    }
-
-  ssd1306TurnOff();
-
+  oledTest();
   ledBlinkTest(30);
   for (;;)
     ledWaveTest();
 }
 
-//--- OLED Display ---------------------------
-/*
-128x64 OLED display
-I2C
-SSD1306
+//--- Oled -----------------------------------
 
-PB5 - OLED RST
-PB6 - OLED SCL
-PB7 - OLED SDA
+void oledTest() {
+  import std.range: iota, chain, retro, drop;
 
-*/
-
-void i2cSetup()
-{
-  rcc_periph_clock_enable(RCC_GPIOB);
-  rcc_periph_clock_enable(RCC_I2C1);
-
-  gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-    GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
-    GPIO_I2C1_SDA); //PB7
-  gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-    GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
-    GPIO_I2C1_SCL); //PB6
-
-  i2c_reset(I2C1);
-  i2c_peripheral_disable(I2C1);
-  i2c_set_speed(I2C1, i2c_speeds.i2c_speed_sm_100k,
-    I2C_CR2_FREQ_36MHZ);
-  i2c_peripheral_enable(I2C1);
-
-  gpio_set_mode(
-    GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-    GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
-
-  gpio_set(GPIOB, GPIO5);
-  sleep(1);
-  gpio_clear(GPIOB, GPIO5);
-  sleep(10);
-  gpio_set(GPIOB, GPIO5);
+  SSD1306!(128,64) oled;
+  oled.init();
+  enum r = iota(1, 26);
+  enum wave = r.chain(r.retro.drop(1));
+  foreach(_; 0 .. 2)
+    foreach(x; wave) {
+      oled.clearScreen();
+      oled.fillRect(
+        x, x, 127-2*x, 62-2*x,
+        Color.WHITE);
+      oled.fillRect(
+        x+5, x+5, 117-2*x, 52-2*x,
+        Color.BLACK);
+      sleep(30);
+      oled.refresh();
+    }
+  oled.turnOff();
 }
 
 //--- Led ------------------------------------
@@ -150,16 +114,7 @@ void timerSetup() {
 
 //--- ssd1306 --------------------------------
 
-enum BLACK   =  0;
-enum WHITE   =  1;
-enum INVERSE =  2;
-
-enum MULTILAYER =  1;
-enum LAYER0     =  1;
-enum LAYER1     =  2;
-
-enum SSD1306_LCDWIDTH  = 128;
-enum SSD1306_LCDHEIGHT = 	64;
+enum Color {BLACK, WHITE, INVERSE};
 
 enum SSD1306_ADDRESS               =  0x3c;
 // Commands
@@ -189,260 +144,218 @@ enum SSD1306_SWITCHCAPVCC          =  0x2;
 enum SSD1306_COLUMNADDR            =  0x21;
 enum SSD1306_PAGEADDR              =  0x22;
 
-shared ubyte[SSD1306_LCDWIDTH * SSD1306_LCDHEIGHT / 8] buffer;
-
-void  ssd1306Init()
+struct SSD1306(int WIDTH = 128,
+               int HEIGHT = 64)
 {
-//   _font = (FONT_INFO*)&ubuntuMono_24ptFontInfo;
-//
-  // Initialisation sequence
-  ssd1306TurnOff();
-  //  1. set mux ratio
-  ssd1306Command!(SSD1306_SETMULTIPLEX);
-  ssd1306Command!(0x3F);
-  //  2. set display offset
-  ssd1306Command!(SSD1306_SETDISPLAYOFFSET);
-  ssd1306Command!(0x0);
-  //  3. set display start line
-  ssd1306Command!(SSD1306_SETSTARTLINE);
-  ssd1306Command!(SSD1306_MEMORYMODE); // 0x20
-  ssd1306Command!(0x00);               // 0x0 act like ks0108
-  //  4. set Segment re-map A0h/A1h
-  ssd1306Command!(SSD1306_SEGREMAP | 0x1);
-  //   5. Set COM Output Scan Direction C0h/C8h
-  ssd1306Command!(SSD1306_COMSCANDEC);
-  //  6. Set COM Pins hardware configuration DAh, 12
-  ssd1306Command!(SSD1306_SETCOMPINS);
-  ssd1306Command!(0x12);
-  //  7. Set Contrast Control 81h, 7Fh
-  ssd1306Command!(SSD1306_SETCONTRAST);
-  ////if (vccstate == SSD1306_EXTERNALVCC)
-  ////  ssd1306Command!(0x9F);
-  ////else
-    ssd1306Command!(0xff);
-  //  8. Disable Entire Display On A4h
-  ssd1306Command!(SSD1306_DISPLAYALLON_RESUME);
-  //  9. Set Normal Display A6h
-  ssd1306Command!(SSD1306_NORMALDISPLAY);
-  //  10. Set Osc Frequency  D5h, 80h
-  ssd1306Command!(SSD1306_SETDISPLAYCLOCKDIV);
-  ssd1306Command!(0x80);
-  //  11. Enable charge pump regulator 8Dh, 14h
-  ssd1306Command!(SSD1306_CHARGEPUMP );
-  ////if (vccstate == SSD1306_EXTERNALVCC)
-  ////  ssd1306Command!(0x10);
-  ////else
-  ssd1306Command!(0x14);
-  //  12. Display On AFh
-  ssd1306TurnOn();
-}
+  enum bufferLen = WIDTH * HEIGHT / 8;
+  ubyte[bufferLen] buffer;
 
-void ssd1306Refresh()
-{
+  void init() {
+    i2cSetup();
+    // Initialisation sequence
+    turnOff();
+    // 1. set mux ratio
+    command!(SSD1306_SETMULTIPLEX);
+    command!(0x3F);
+    // 2. set display offset
+    command!(SSD1306_SETDISPLAYOFFSET);
+    command!(0x0);
+    // 3. set display start line
+    command!(SSD1306_SETSTARTLINE);
+    command!(SSD1306_MEMORYMODE);
+    command!(0x00);
+    // 4. set Segment re-map A0h/A1h
+    command!(SSD1306_SEGREMAP | 0x1);
+    // 5. Set COM Output Scan Direction
+    //    C0h/C8h
+    command!(SSD1306_COMSCANDEC);
+    // 6. Set COM Pins hardware configuration
+    //    DAh, 12
+    command!(SSD1306_SETCOMPINS);
+    command!(0x12);
+    // 7. Set Contrast Control 81h, 7Fh
+    command!(SSD1306_SETCONTRAST);
+    command!(0xff);
+    // 8. Disable Entire Display On A4h
+    command!(SSD1306_DISPLAYALLON_RESUME);
+    // 9. Set Normal Display A6h
+    command!(SSD1306_NORMALDISPLAY);
+    // 10. Set Osc Frequency  D5h, 80h
+    command!(SSD1306_SETDISPLAYCLOCKDIV);
+    command!(0x80);
+    // 11. Enable charge pump regulator 8Dh,
+    //     14h
+    command!(SSD1306_CHARGEPUMP );
+    command!(0x14);
+    // 12. Display On AFh
+    turnOn();
+    turnOff();
+    turnOn();
+  }
 
-  ssd1306Command!(SSD1306_COLUMNADDR); // 0x21 COMMAND
-  ssd1306Command!(0); // Column start address
-  ssd1306Command!(SSD1306_LCDWIDTH-1); // Column end address
+  void i2cSetup() {
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_I2C1);
 
-  ssd1306Command!(SSD1306_PAGEADDR); // 0x22 COMMAND
-  ssd1306Command!(0); // Start Page address
-  ssd1306Command!((SSD1306_LCDHEIGHT/8)-1);// End Page address
+    gpio_set_mode(GPIOB,
+      GPIO_MODE_OUTPUT_50_MHZ,
+      GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+      GPIO_I2C1_SDA); //PB7
+    gpio_set_mode(GPIOB,
+      GPIO_MODE_OUTPUT_50_MHZ,
+      GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+      GPIO_I2C1_SCL); //PB6
 
-  ubyte[1+SSD1306_LCDWIDTH * SSD1306_LCDHEIGHT / 8] buf;
-  buf[1..$] = buffer[];
-  buf[0] = 0x40;
-  i2c_transfer7(I2C1, SSD1306_ADDRESS,
-                buf.ptr, buf.length, null, 0);
-}
+    i2c_reset(I2C1);
+    i2c_peripheral_disable(I2C1);
+    i2c_set_speed(I2C1,
+      i2c_speeds.i2c_speed_sm_100k,
+      I2C_CR2_FREQ_36MHZ);
+    i2c_peripheral_enable(I2C1);
 
-void ssd1306TurnOn()
-{
-  ssd1306Command!(SSD1306_DISPLAYON);
-}
+    gpio_set_mode(
+      GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+      GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
 
-void ssd1306TurnOff()
-{
-  ssd1306Command!(SSD1306_DISPLAYOFF);
-}
+    gpio_set(GPIOB, GPIO5);
+    sleep(1);
+    gpio_clear(GPIOB, GPIO5);
+    sleep(10);
+    gpio_set(GPIOB, GPIO5);
+  }
 
-void  ssd1306Command(int comm)()
-{
-  ubyte[2] buf;
-  buf[0] = 0x00;
-  buf[1] = comm;
-  i2c_transfer7(I2C1, SSD1306_ADDRESS,
-                buf.ptr, 2, null, 0);
-}
+  void command(int comm)() {
+    ubyte[2] buf =[0x00, comm];
+    i2c_transfer7(I2C1, SSD1306_ADDRESS,
+                  buf.ptr, 2, null, 0);
+  }
 
-void ssd1306ClearScreen()
-{
-  buffer[] = 0x00;
-}
+  void refresh() {
+    command!(SSD1306_COLUMNADDR);
+    command!(0);
+    command!(WIDTH-1);
+    command!(SSD1306_PAGEADDR);
+    command!(0);
+    command!((HEIGHT/8)-1);
 
-void ssd1306DrawPixel(int x, int y, int color)
-{
-  import core.atomic: atomicOp;
+    ubyte[1+bufferLen] buf;
+    buf[0] = 0x40;
+    buf[1 .. $] = buffer[];
+    i2c_transfer7(I2C1, SSD1306_ADDRESS,
+      buf.ptr, buf.length, null, 0);
+  }
 
-  if (x < 0)
-    return;
-  if (y < 0)
-    return;
-  if (x >= SSD1306_LCDWIDTH)
-    return;
-  if (y >= SSD1306_LCDHEIGHT)
-    return;
+  void turnOn() {
+    command!(SSD1306_DISPLAYON);
+  }
 
-  auto bytePos = x + (y / 8) * SSD1306_LCDWIDTH;
-  auto bit = 1 << (y & 7);
-  switch (color)
+  void turnOff() {
+    command!(SSD1306_DISPLAYOFF);
+  }
+
+  void clearScreen() {
+    buffer[] = 0x00;
+  }
+
+  void drawPixel(int x, int y, int color) {
+    if (x < 0) return;
+    if (y < 0) return;
+    if (x >= WIDTH) return;
+    if (y >= HEIGHT) return;
+
+    auto bytePos = x + (y/8) * WIDTH;
+    auto bit = 1 << (y & 7);
+    switch (color)
+    {
+      case Color.WHITE  :
+        buffer[bytePos] |= bit;
+        break;
+      case Color.BLACK  :
+        buffer[bytePos] &= ~bit;
+        break;
+      case Color.INVERSE:
+        buffer[bytePos] ^= bit;
+        break;
+      default:
+    }
+  }
+
+  void SWAP(ref int a, ref int b)
   {
-    case WHITE  : buffer[bytePos].atomicOp!"|="( bit); break;
-    case BLACK  : buffer[bytePos].atomicOp!"&="(~bit); break;
-    case INVERSE: buffer[bytePos].atomicOp!"^="( bit); break;
-    default:
-  }
-}
-
-void SWAP(ref short a, ref short b)
-{
-  short t = a;
-  a = b;
-  b = t;
-}
-
-short abs(int v) {
-  return cast(short)((v < 0) ? -v : v);
-}
-
-void ssd1306DrawLine(short x0, short y0,
-                     short x1, short y1,
-                     ushort color)
-{
-  short steep = (abs(y1 - y0) > abs(x1 - x0));
-  if (steep) {
-    SWAP(x0, y0);
-    SWAP(x1, y1);
+    int t = a;
+    a = b;
+    b = t;
   }
 
-  if (x0 > x1) {
-    SWAP(x0, x1);
-    SWAP(y0, y1);
+  int abs(int v) {
+    return (v < 0) ? -v : v;
   }
 
-  short dx, dy;
-  dx = cast(short)(x1 - x0);
-  dy = abs(y1 - y0);
-
-  short err   = dx / 2;
-  short ystep;
-
-  if (y0 < y1) {
-    ystep = 1;
-  } else {
-    ystep = -1;
-  }
-
-  for (; x0 <= x1; x0++) {
+  void drawLine(int x0, int y0,
+                int x1, int y1,
+                int color) {
+    int steep = (abs(y1 - y0) > abs(x1 - x0));
     if (steep) {
-      ssd1306DrawPixel(y0, x0, color);
+      SWAP(x0, y0);
+      SWAP(x1, y1);
+    }
+    if (x0 > x1) {
+      SWAP(x0, x1);
+      SWAP(y0, y1);
+    }
+
+    int dx = x1 - x0;
+    int dy = abs(y1 - y0);
+
+    int err = dx / 2;
+    int ystep;
+
+    if (y0 < y1)
+      ystep = 1;
+    else
+      ystep = -1;
+
+    for (; x0 <= x1; x0++) {
+      if (steep)
+        drawPixel(y0, x0, color);
+      else
+        drawPixel(x0, y0, color);
+      err -= dy;
+      if (err < 0) {
+        y0 += ystep;
+        err += dx;
+      }
+    }
+  }
+
+  void fillRect(int x, int y,
+                int w, int h,
+                int color) {
+    foreach(yy; y .. y+h)
+      foreach(xx; x .. x+w)
+        drawPixel(xx, yy, color);
+  }
+
+  void drawRect(int x, int y,
+                int w, int h,
+                int color) {
+    if (w == 0)
+      return;
+    if (h == 0)
+      return;
+    int x1 = x + w - 1;
+    int y1 = y + h - 1;
+    if ((h > 2 ) | (w > 2)) {
+      drawLine(x,    y, x1,    y, color);
+      drawLine(x,   y1, x1,   y1, color);
+      drawLine(x,  y+1,  x, y1-1, color);
+      drawLine(x1, y+1, x1, y1-1, color);
     } else {
-      ssd1306DrawPixel(x0, y0, color);
-    }
-    err -= dy;
-    if (err < 0) {
-      y0 += ystep;
-      err += dx;
+      drawLine(x,  y, x1,  y, color);
+      drawLine(x, y1, x1, y1, color);
     }
   }
+
 }
-
-// void  ssd1306SetFont(FONT_INFO * f) {
-//     _font = f;
-// }
-
-void ssd1306FillRect(int x, int y, int w, int h,
-                     int color)
-{
-  int x0 = x;
-  int x1 = x + w;
-  int y1 = y + h;
-  for(; y < y1; y++)
-    for(x = x0; x < x1; x++)
-      ssd1306DrawPixel(x, y, color);
-}
-
-void ssd1306DrawRect(short x, short y, short w, short h,
-                     ushort color)
-{
-  if (w == 0)
-    return;
-  if (h == 0)
-    return;
-  short x1 = cast(short)(x + w - 1);
-  short y1 = cast(short)(y + h - 1);
-  short yp1 = cast(short)(y + 1);
-  short y1m1 = cast(short)(y1 - 1);
-
-  if((h > 2 ) | (w > 2)) {
-    ssd1306DrawLine(x,    y, x1,    y, color);
-    ssd1306DrawLine(x,   y1, x1,   y1, color);
-    ssd1306DrawLine(x,  yp1,  x, y1m1, color);
-    ssd1306DrawLine(x1, yp1, x1, y1m1, color);
-  } else {
-    ssd1306DrawLine(x,  y, x1,  y, color);
-    ssd1306DrawLine(x, y1, x1, y1, color);
-  }
-}
-
-// short  ssd1306DrawChar(short x, short y,
-//                          ubyte c, ubyte size,
-//                          ushort color)
-// {
-//     short i,j,k, _x;
-//     ushort line;
-//
-//     if( (c < _font->startChar) ||   // skip if character don't exist
-//         (c > _font->endChar))        // skip if character don't exist
-//     return 0;
-//
-//     c = c - _font->startChar;
-//     // skip invisible characters
-//     line = _font->charInfo[c].offset;
-//     // scan height
-//     for ( i=0; i < _font->charInfo[c].heightBits; i++ ) {
-//         k =  (_font->charInfo[c].widthBits-1)/8 + 1; //number of bytes in a row
-//         _x = 0;
-//         do {
-//             short l = _font->data[line];
-//             // scan width
-//             for ( j = 0; j < 8; j++ ) {
-//                 if ( l & 0x80 ) {
-//                     if (size == 1) {
-//                         ssd1306DrawPixel( x+_x, y+i, color, layer );
-//                     } else {
-//                         ssd1306DrawRect(x+(_x*size), y+(i*size), size, size, color, layer);
-//                     }
-//                 }
-//                 l <<= 1;
-//                 _x++;
-//             }
-//         k--;
-//         line++;
-//         } while (k > 0);
-//     }
-//     //    ssd1306Refresh();
-//     return _font->charInfo[c].widthBits; // return characher width
-// }
-//
-// void  ssd1306DrawString(short x, short y, int8_t *text, ubyte size,
-//                         ushort color, ushort layer)
-// {
-//   static ushort l, pos;
-//   pos =  0;
-//   for (l = 0; l < strlen(text); l++)
-//   {
-//     pos = pos + ssd1306DrawChar(x + (pos * size + 1), y, text[l], size, color, layer);
-//   }
-// }
 
 //--------------------------------------------
