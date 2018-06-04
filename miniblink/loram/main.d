19110563979687f88bb4f7d1049877f8d6bbec0c
wiffel <wiffel@tilient.net>
2018-06-04 23:29:47 +0200

    no idea

--- miniblink/loram/main.d
+++ miniblink/loram/main.d
@@ -8,9 +8,8 @@ extern(C) void main()
 {
   timerSetup();
   auto led  = IO(GPIOC, GPIO13, RCC_GPIOC);
-  auto oled = SSD1306(128, 64,
-                      GPIOB, GPIO5, RCC_GPIOB,
-                      I2C1, 0x3C);
+  auto oled = SSD1306(128, 64, I2C1, 0x3C,
+                      GPIOB, GPIO5, RCC_GPIOB);
   for (;;)
   {
     oled.test();
@@ -57,24 +56,24 @@ void test(ref IO led, int seconds = 1)
 
 void waveTest(ref IO led, int seconds = 2)
 {
-  import std.range: iota, retro, chain, drop;
+  import std.range: iota, retro, chain;
+  immutable max = 25;
 
-  immutable max   = 25;
   void onOff(int onTime)
   {
+    led.on();
+    sleepMs(onTime);
+    led.off();
+    sleepMs(max - onTime);
   }
+
   auto steps = iota(1, max);
-  auto wave  = steps.chain(steps.retro.drop(1));
+  auto wave  = steps.chain(steps.retro);
   foreach (_; 0 .. 3)
   {
     foreach(onTime; wave)
-    {
-      led.on();
-      sleepMs(onTime);
-      led.off();
-      sleepMs(max - onTime);
-    }
-    sleepMs(seconds * 1000 - max * (2 * max - 3));
+      onOff(onTime);
+    sleepMs(seconds * 1000 - max * (2 * max - 2));
   }
 }
 
@@ -148,8 +147,8 @@ struct SSD1306
   alias gfx this;
 
   this(int width, int height,
-       uint rstPort, ushort rstPin, int clk,
-       uint i2c, ubyte i2cAddr)
+       uint i2c, ubyte i2cAddr,
+       uint rstPort, ushort rstPin, int clk)
   {
     this.i2c = i2c;
     this.i2cAddr = i2cAddr;
@@ -192,10 +191,9 @@ struct SSD1306
 
   void command(ubyte[] comm ...)
   {
-    ubyte[2] buf = [0x00u, 0x00u];
     foreach(c; comm)
     {
-      buf[1] = c;
+      ubyte[2] buf = [0x00u, c];
       i2c_transfer7(this.i2c, this.i2cAddr,
                     buf.ptr, 2, null, 0);
     }
@@ -203,11 +201,13 @@ struct SSD1306
 
   void refresh()
   {
-    command(0x21, 0x00, cast(ubyte)(width - 1),
-            0x22, 0x00, cast(ubyte)((height / 8) - 1));
-    gfx.buffer[0] = 0x40;
+    this.command(
+      0x21, 0x00, cast(ubyte) (width - 1),
+      0x22, 0x00, cast(ubyte) ((height / 8) - 1));
+    this.gfx.buffer[0] = 0x40;
     i2c_transfer7(this.i2c, this.i2cAddr,
-      gfx.buffer.ptr, gfx.buffer.length, null, 0);
+      this.gfx.buffer.ptr, this.gfx.buffer.length,
+      null, 0);
   }
 
   void turnOn()
@@ -238,7 +238,7 @@ struct Gfx
     this.width = width;
     this.height = height;
     immutable len = 1 + width * height / 8;
-    this.buffer = (cast(ubyte*)malloc(len))[0..len];
+    this.buffer = (cast(ubyte*) malloc(len))[0..len];
     this.buffer[] = 0x00u;
   }
 
