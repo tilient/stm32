785f4ecb844ea70397b8b4b527a5ddd6dab48a8d
wiffel <wiffel@tilient.net>
2018-08-24 11:41:53 +0200

    Rust version refactorings

--- miniblink/rust/bluepill/src/main.rs
+++ miniblink/rust/bluepill/src/main.rs
@@ -5,19 +5,18 @@
 #![feature(panic_implementation)]
 #![feature(core_intrinsics)]
 
-use core::ptr;
-use core::panic::PanicInfo;
 
-//**********************************************************
-/* to build ...
+/********************************************************* /
+to build :
+
 cargo build --release
 arm-none-eabi-objcopy -Obinary \
   target/thumbv7m-none-eabi/release/bluepill blink.bin
-*/
-//**********************************************************
+/ *********************************************************/
 
 #[no_mangle]
-pub extern "C" fn main() {
+pub extern "C" fn main()
+{
   init_gpio();
   loop {
     set_port(13);
@@ -31,27 +30,53 @@ pub extern "C" fn main() {
 //** SysTick Timer
 //**********************************************************
 
-const SYSTICK_CSR: *mut u32 = 0xE000_E010 as *mut u32;
-const SYSTICK_RVR: *mut u32 = 0xE000_E014 as *mut u32;
-const SYSTICK_VAL: *mut u32 = 0xE000_E018 as *mut u32;
-const TCK_FLG: u32 = 0x10_000;
+fn sleep(millis: u32)
+{
+  const CLK_FREQ: u32 = 72_000_000;
+  const MS_TICKS: u32 = CLK_FREQ / 9_000; // ??
+  const MAX_MS: u32 = 0x00FF_FFFF / MS_TICKS;
 
-fn sleep(millis: u32) {
+  const SYSTICK_CSR: *mut u32 = 0xE000_E010 as *mut u32;
+  const SYSTICK_RVR: *mut u32 = 0xE000_E014 as *mut u32;
+  const SYSTICK_VAL: *mut u32 = 0xE000_E018 as *mut u32;
+  const TCK_FLG: u32 = 0x10_000;
+
   let mut ms = millis;
-  while ms > 2000 {
-    sleep(2000);
-    ms -= 2000;
+  while ms > MAX_MS {
+    sleep(MAX_MS);
+    ms -= MAX_MS;
   }
-  unsafe {
-    ptr::write_volatile(SYSTICK_CSR, 4);
-    ptr::write_volatile(SYSTICK_RVR, ms * 8_000);
-    ptr::write_volatile(SYSTICK_VAL, 0);
-    ptr::write_volatile(SYSTICK_CSR, 7);
-    while 0 == (ptr::read_volatile(SYSTICK_CSR) & TCK_FLG) {
-      asm!("wfi");
-    }
-    ptr::write_volatile(SYSTICK_CSR, 4);
+  ptr_write(SYSTICK_CSR, 4);
+  ptr_write(SYSTICK_RVR, ms * MS_TICKS);
+  ptr_write(SYSTICK_VAL, 0);
+  ptr_write(SYSTICK_CSR, 7);
+  while 0 == (ptr_read(SYSTICK_CSR) & TCK_FLG) {
+    wfi();
   }
+  ptr_write(SYSTICK_CSR, 4);
+}
+
+//**********************************************************
+//** Tools
+//**********************************************************
+
+fn ptr_write(p: *mut u32, v: u32)
+{
+  use core::ptr;
+
+  unsafe { ptr::write_volatile(p, v); }
+}
+
+fn ptr_read(p: *mut u32) -> u32
+{
+  use core::ptr;
+
+  unsafe { ptr::read_volatile(p) }
+}
+
+fn wfi()
+{
+  unsafe { asm!("wfi"); }
 }
 
 //**********************************************************
@@ -60,29 +85,27 @@ fn sleep(millis: u32) {
 
 // LED0 = PC13;
 
-const RCC_APB2ENR: *mut u32 = 0x4002_1018 as *mut u32;
-const RCC_APB2ENR_IOPCEN: u32 = 1 << 4;
-const GPIOC_CRH: *mut u32 = 0x4001_1004 as *mut u32;
+fn init_gpio()
+{
+  const RCC_APB2ENR: *mut u32 = 0x4002_1018 as *mut u32;
+  const RCC_APB2ENR_IOPCEN: u32 = 1 << 4;
+  const GPIOC_CRH: *mut u32 = 0x4001_1004 as *mut u32;
+
+  ptr_write(RCC_APB2ENR,
+            ptr_read(RCC_APB2ENR) | RCC_APB2ENR_IOPCEN);
+  ptr_write(GPIOC_CRH, 0x44544444);
+}
+
 const GPIOC_BSRR: *mut u32 = 0x4001_1010 as *mut u32;
 
-fn init_gpio() {
-  unsafe {
-    ptr::write_volatile(RCC_APB2ENR,
-      ptr::read_volatile(RCC_APB2ENR) | RCC_APB2ENR_IOPCEN);
-    ptr::write_volatile(GPIOC_CRH, 0x44544444);
-  }
+fn set_port(p: u8)
+{
+  ptr_write(GPIOC_BSRR, 1 << (p + 16));
 }
 
-fn set_port(p: u8) {
-  unsafe {
-    ptr::write_volatile(GPIOC_BSRR, 1 << (p + 16));
-  }
-}
-
-fn clear_port(p: u8) {
-  unsafe {
-    ptr::write_volatile(GPIOC_BSRR, 1 << p);
-  }
+fn clear_port(p: u8)
+{
+  ptr_write(GPIOC_BSRR, 1 << p);
 }
 
 //**********************************************************
@@ -90,10 +113,11 @@ fn clear_port(p: u8) {
 //**********************************************************
 
 pub type Handler = unsafe extern "C" fn();
+pub type Handlers = [Option<Handler>; 15];
 
-#[link_section = ".vector.exceptions"]
 #[no_mangle]
-pub static EXCEPTION_HANDLERS: [Option<Handler>; 15] = [
+#[link_section = ".vector.exceptions"]
+pub static EXCEPTION_HANDLERS: Handlers = [
   Some(_reset),
   Some(_nmi),
   Some(_hard_fault),
@@ -123,17 +147,20 @@ extern "C" {
 
 #[doc(hidden)]
 #[export_name = "_systick"]
-pub unsafe extern "C" fn systick() {
+pub unsafe extern "C" fn systick()
+{
 }
 
 #[doc(hidden)]
 #[export_name = "_default_exception_handler"]
-pub unsafe extern "C" fn default_handler_entry_point() {
+pub unsafe extern "C" fn default_handler_entry_point()
+{
 }
 
 #[doc(hidden)]
 #[export_name = "_reset"]
-pub unsafe extern "C" fn reset() {
+pub unsafe extern "C" fn reset()
+{
   main();
 }
 
@@ -141,9 +168,13 @@ pub unsafe extern "C" fn reset() {
 //** Panic
 //**********************************************************
 
+use core::panic::PanicInfo;
+
 #[panic_implementation]
-fn panic(_info: &PanicInfo) -> !  {
+fn panic(_info: &PanicInfo) -> !
+{
   use core::intrinsics;
+
   unsafe { intrinsics::abort() }
 }
 
